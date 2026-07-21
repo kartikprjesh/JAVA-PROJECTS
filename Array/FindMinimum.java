6e88797c7948a3060d9f74f8a55b3602949d2bfa
kartikprjesh <kartikprjesh@gmail.com>
2026-07-21 12:27:13 +0530

    Add Java Array practice programs

---
+++ Array/FindMinimum.java
@@ -0,0 +1,23 @@
+import java.util.*;
+public class FindMinimum {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the size of element...");
+        int n=sc.nextInt();
+        int arr[]=new int[n];
+        System.out.println("Enter element of array...");
+        for(int i=0;i<n;i++){
+            arr[i]=sc.nextInt();
+        }
+
+        int min=arr[0];
+        for(int i=0;i<n;i++){
+        if(arr[i] < min){
+            min=arr[i];
+        }
+
+        }
+        System.out.println("minimum is:"+min);
+    }
+    
+}
